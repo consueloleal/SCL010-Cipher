423148fe3a1676304fc3582a25c13d19ed25705b
Consuelo Leal <consuelo.leal@outlook.com>
2019-07-01 09:41:28 -0400

    proyecto cipher completo

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,31 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (offset, messageToEncode) => {
+if (offset !== undefined && messageToEncode !== undefined){
+	  	encrytMessage = ""
+	  	for (var i = 0; i < messageToEncode.length; i++) {
+	  	  if (messageToEncode.charAt(i).match(/[a-z]/i) !== null) {
+	  	    asciiNumber  = messageToEncode.charAt(i).toUpperCase().charCodeAt();
+	  	    valueAscii = (asciiNumber - 65 + parseInt(offset)) % 26 + 65;
+	  	  	encrytMessage += String.fromCharCode(valueAscii);
+		 	  }else if ( messageToEncode.charAt(i) === " ") {
+	 
+	  	  	encrytMessage += " ";
+	  	  }
+	  	}
+  	}
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
+  decode: (offset, messageToDecode) => {
+   	if (offset !== undefined) {
+	  	encryptedMessage= "";
+	  	for (var i = 0; i < messageToDecode.length; i++) {
+	  	  if (messageToDecode.charAt(i).match(/[a-z]/i) !== null) {
+	  	    asciiNumber  = messageToDecode.charAt(i).toUpperCase().charCodeAt();
+	  	    valueAscii = (asciiNumber + 65 - parseInt(offset)) % 26 + 65;
+	  	    encryptedMessage += String.fromCharCode(valueAscii);
+	  		}else if ( messageToDecode.charAt(i) === " ") {
+	  	    encryptedMessage += " ";
+	  	  }
+	  	}
+  	}
+  }, 	
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-/*Acá va tu código*/
+const btnEncode = document.getElementById('btnEncode');
+const btnDecode = document.getElementById('btnDecode');
+const btnClean = document.getElementById('cleanTextArea');
+const btnCopyText = document.getElementById('copyTextArea');
+
+let messageToEncode,  messageToDecode;
+
+btnEncode.addEventListener('click', () => {
+	let offsetNumber = document.getElementById('offsetNumber').value;
+	let toEncode = document.getElementById('messageDecodeEncode').value;
+	messageToEncode = window.cipher.encode(offsetNumber, toEncode);
+	document.getElementById('readResult').value = encrytMessage;
+
+
+});
+
+btnDecode.addEventListener('click', () => {
+	let offsetNumber = document.getElementById('offsetNumber').value;
+	let toDecode = document.getElementById('messageDecodeEncode').value;
+	 messageToDecode = window.cipher.decode(offsetNumber, toDecode);
+	document.getElementById('readResult').value = encryptedMessage;
+});
+
+
+btnClean.addEventListener('click', () => {
+  document.getElementById('offsetNumber').value = "";
+  document.getElementById('messageDecodeEncode').value = "";
+  document.getElementById('readResult').value="";
+  });
+
+btnCopyText.addEventListener('click', () => {
+  document.getElementById('readResult').select();
+  document.execCommand('copy');
+});
+
+
+
+
+
